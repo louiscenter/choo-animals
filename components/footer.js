849450c7354478db9f1ffb69537be739dff06e46
Louis Center <git@louis.center>
2017-04-02 21:15:33 +1000

    Updated footer

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ var html = require('choo/html')
 module.exports = function () {
   return html`
     <footer>
-      made by <a href="https://github.com/louiscenter">@louiscenter</a>
+      made by <a href="https://twitter.com/louiscenter">@louiscenter</a> with <a href="https://github.com/yoshuawuyts/choo">choo</a>
     </footer>
   `
 }
